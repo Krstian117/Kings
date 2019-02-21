5175054c88bdd68ca5c40444395165aa9e0d99b1
SISTRAN\cgonzalez <cgonzalez@sistran.com.co>
2019-02-21 10:33:45 -0500

    Agregar archivos de proyecto.

---
+++ Kings/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kings.MvcApplication" Language="C#" %>
---
+++ Kings/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kings.Startup))]
+namespace Kings
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
